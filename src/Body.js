573877149c78e75cc8b4a2725699e18946b0e7ed
Deepali <deepalijindal04@gmail.com>
2020-01-11 23:44:13 +0530

    api to get coins

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Api from './Api.js';
+
 
 function Body(){
     return (
         <div>
            <center><p>Search for a coin</p></center>
+           <Api />
             
         </div>
     );
